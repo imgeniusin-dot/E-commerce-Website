5288dfd3f7ab842118870f1a60ccba0a569a8b90
imgeniusin-dot <imgeniusin@gmail.com>
2026-04-04 20:54:37 +0530

    Remove unnecessary comment from settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent   ✅ pehle
+BASE_DIR = Path(__file__).resolve().parent.parent  
 
 STATIC_URL = '/static/'
 
