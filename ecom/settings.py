f7235a614a93c81bb3f389ee9a8ed23e4def9b55
imgeniusin-dot <imgeniusin@gmail.com>
2026-04-08 18:49:29 +0530

    Update STATICFILES_DIRS to use Path operator

--- ecom/settings.py
+++ ecom/settings.py
@@ -13,14 +13,14 @@ import dj_database_url
 from pathlib import Path
 import os
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent  
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "static"
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
